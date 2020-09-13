7db1bde51f5c9fefd1458459c0b561ebe9304b58
Skyler Sidner <MalevolentDragon@gmail.com>
2020-09-13 12:33:33 -0500

    Confirm reset feature (#7)
    * testing commit on develop
    
    * Feature/ace high (#1)
    
    * moving ace low to high
    
    * adjusting Ace from H to A
    
    * adding .DS_Store to gitignore
    
    * Feature/ace high (#4)
    
    * moving ace low to high
    
    * adjusting Ace from H to A
    
    * created confirm modal for reset button
    
    * adding id to header
    
    * feature: adding reset confirmation to ace high toggle (#6)
    
    Co-authored-by: Skyler A Sidner <skylerasidner@Skylers-MacBook-Pro-3.local>
    Co-authored-by: Dan H <dan.haske@yahoo.com>

--- src/script.js
+++ src/script.js
@@ -159,17 +159,34 @@ $(function() {
   }
 
   function bindResetButton() {
-    $('#reset').on('click', () => resetTable())
+    $('#reset').on('click', () => {
+      const isConfirmed = confirmReset()      
+      if (isConfirmed) {
+        resetTable()
+      }
+    })
   }
 
   function createAceHighLowToggle() {
     const toggle = $('<input type="checkbox" id="ace-high-toggle">Ace High (will reset form)</input>')
-    toggle.on('click', () => buildTrackingTable())
+    toggle.on('click', () => onAceToggleClick())
 
     toggle.insertAfter('#header')
   }
 
+  function onAceToggleClick() {
+    const isConfirmed = confirmReset()
+    if (isConfirmed) {
+      buildTrackingTable()
+    }
+  }
+  
+  function confirmReset() {
+    return confirm("Are you sure you want to reset the table?");
+  } 
+  
   createAceHighLowToggle()
   buildTrackingTable()
   bindResetButton()
 })
+
