c8f394e7a0b01930f954ed626dfbf1883b6ec90e
Dan <dan.haske@yahoo.com>
2020-10-25 11:02:33 -0500

    express server now serves index.html

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const express = require('express')
+const path = require('path')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send({ data: 'Hello World! and Good Morn!' })
-})
+const staticPath = path.join(__dirname, '/src');
+app.use(express.static(staticPath));
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+  console.log(`Playing-card-tracker-plus listening at http://localhost:${port}`)
 })
